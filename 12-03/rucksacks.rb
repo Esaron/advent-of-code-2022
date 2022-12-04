c872c3c02123a08d2b8260bebf4683d6de8b3afc
Jon Marshall <jon.marshall@betterup.co>
2022-12-04 16:07:36 -0500

    Day 3 Part 2
    Find the common item amongst groups of 3 rucksacks and sum the priorities of all such items.

--- 12-03/rucksacks.rb
+++ 12-03/rucksacks.rb
@@ -6,9 +6,8 @@ def priority(chr)
   chr == chr.upcase ? chr.ord - 38 : chr.ord - 96
 end
 
-total = File.readlines(INPUT).sum(0) do |line|
-  first_compartment = line.chars
-  second_compartment = first_compartment.slice!(..(first_compartment.length/2 - 1))
-  priority(first_compartment.intersection(second_compartment).first)
+total = File.readlines(INPUT).each_slice(3).sum do |group|
+  common_item = group.map(&:chars).reduce { |common_items, rucksack| common_items.intersection(rucksack) }.first
+  priority(common_item)
 end
 puts total
