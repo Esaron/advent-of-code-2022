3bc715675aa1d3b78cb81a8417b8b9798203bddb
Jon Marshall <jon.marshall@betterup.co>
2022-12-04 16:25:38 -0500

    Day 4, Part 2
    Find count of overlaps in pairs of ranges.

--- 12-04/cleaning_assignments.rb
+++ 12-04/cleaning_assignments.rb
@@ -7,12 +7,16 @@ class Range
   def contains?(range)
     cover?(range.first) && cover?(range.last)
   end
+
+  def overlaps?(range)
+    cover?(range.first) || range.cover?(first)
+  end
 end
 
 count = File.readlines(INPUT).count do |line|
   elf1_start, elf1_end, elf2_start, elf2_end = line.match(LINE_REGEX).captures.map(&:to_i)
   elf1_sections = elf1_start..elf1_end
   elf2_sections = elf2_start..elf2_end
-  elf1_sections.contains?(elf2_sections) || elf2_sections.contains?(elf1_sections)
+  elf1_sections.overlaps?(elf2_sections)
 end
 puts count
