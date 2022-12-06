c9b76b0afd317da926c620b70618602d11f16522
Jon Marshall <jon.marshall@betterup.co>
2022-12-06 11:59:08 -0500

    Day 6, Part 2
    Detect ending index of first n unique characters in a stream.
    Was passing in a reader, which would prevent redundant reads, but
    it only works if subsequent calls are for >n length substrings,
    so just did the redundant call. Could be improved to be a bit more
    performant, but it's fine.
    
    https://adventofcode.com/2022/day/6#part2

--- 12-06/findStreamSequence.groovy
+++ 12-06/findStreamSequence.groovy
@@ -1,11 +1,15 @@
 #!/usr/bin/env groovy
 
 INPUT = 'input.txt'
+FILE = new File(INPUT)
+START_OF_PACKET_MARKER_LENGTH = 4
+START_OF_MESSAGE_MARKER_LENGTH = 14
 
-def index = 0
-new File(INPUT).withReader { reader ->
-  def uniqueChars = []
-  for (char c = reader.read(); c != -1 && uniqueChars.size() != 4; c = reader.read()) {
+def uniqueSequenceEndIndex(file, length) {
+  int index = 0
+  file.withReader { reader ->
+  List<char> uniqueChars = []
+  for (char c = reader.read(); c != -1 && uniqueChars.size() != length; c = reader.read()) {
     def currentIndex = uniqueChars.indexOf(c)
     if (currentIndex != -1) {
       uniqueChars = uniqueChars.drop(currentIndex + 1)
@@ -13,6 +17,9 @@ new File(INPUT).withReader { reader ->
     uniqueChars << c
     index++
   }
+  }
+  return index
 }
 
-println(index)
+println "Part 1: ${uniqueSequenceEndIndex(FILE, START_OF_PACKET_MARKER_LENGTH)}"
+println "Part 2: ${uniqueSequenceEndIndex(FILE, START_OF_MESSAGE_MARKER_LENGTH)}"
